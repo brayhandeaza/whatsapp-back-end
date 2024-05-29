f401b0f6cdbdedfcd8e3c54e9233fab8717ac313
Brayhan De Aza <brayhandeaza@gmail.com>
2024-05-28 22:50:16 -0400

    postgres

--- config/index.ts
+++ config/index.ts
@@ -2,18 +2,22 @@ import "dotenv/config"
 import { Sequelize } from "sequelize";
 import pg from "pg"
 
-export const db = new Sequelize({
-    dialect: 'postgres',
-    username: process.env.LOCAL_USER,
-    password: process.env.LOCAL_PASSWORD,
-    database: process.env.LOCAL_DATABASE,
-    host: process.env.LOCAL_HOST,
-    port: Number(process.env.LOCAL_PORT) || 5432,
-    dialectModule: pg, // I've added this.
-    timezone: process.env.TZ,
-    define: {
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci',
-    },
-    logging: false
-})
+
+// {
+//     dialect: 'postgres',
+    
+//     username: process.env.LOCAL_USER,
+//     password: process.env.LOCAL_PASSWORD,
+//     database: process.env.LOCAL_DATABASE,
+//     host: process.env.LOCAL_HOST,
+//     port: Number(process.env.LOCAL_PORT) || 5432,
+//     dialectModule: pg, // I've added this.
+//     timezone: process.env.TZ,
+//     define: {
+//         charset: 'utf8mb4',
+//         collate: 'utf8mb4_general_ci',
+//     },
+//     logging: false
+// }
+
+export const db = new Sequelize(process.env.LOCAL_URL!);
