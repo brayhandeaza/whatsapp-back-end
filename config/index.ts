6f90902becec8be78822d4efc10607735c45a14f
Brayhan De Aza <brayhandeaza@gmail.com>
2024-05-28 20:31:43 -0400

    db config

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,25 @@
 import "dotenv/config"
 import { Sequelize } from "sequelize";
+import pg from "pg"
+
+
+// export const db = new Sequelize(process.env.POSTGRES_URL!)
 
 
 
-export const db = new Sequelize(process.env.POSTGRES_URL!)
 
+export const db = {
+    dialect: 'postgres',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialectModule: pg, // I've added this.
+    timezone: process.env.TZ,
+    define: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    },
+    logging: false,
+  };
